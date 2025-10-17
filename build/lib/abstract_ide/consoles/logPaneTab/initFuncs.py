4549dbf6a0fdc9b85ed969fe254943e02c02be2d
AbstractEndeavors <partners@abstractendeavors.com>
2025-10-17 14:16:52 -0500

    Deploy version 0.0.0.352 at 2025-10-17 19:16:52 UTC

--- build/lib/abstract_ide/consoles/logPaneTab/initFuncs.py
+++ build/lib/abstract_ide/consoles/logPaneTab/initFuncs.py
@@ -1,10 +1,10 @@
 
 
-from .functions import ()
+from .functions import (append_line,)
 
 def initFuncs(self):
     try:
-        for f in ():
+        for f in (append_line,):
             setattr(self, f.__name__, f)
     except Exception as e:
         logger.info(f"{e}")
