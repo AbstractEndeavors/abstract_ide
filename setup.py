a335dcb88ad65047146d3404abddcdc58a83c6ea
solcatcher <jrputkey55@gmail.com>
2026-07-13 02:31:18 -0500

    Release abstract_ide==0.0.0.395 @ 2026-07-13 07:31:18 UTC

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='abstract_ide',
-    version='0.0.0.394',
+    version='0.0.0.395',
     author='putkoff',
     author_email='partners@abstractendeavors.com',
     description='abstract_ide',
